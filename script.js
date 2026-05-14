4f09585f9da3fa2452dacb6b5ade7b770dde231a
Shiva Kumar Dasarla <77926770+Shivaa2203@users.noreply.github.com>
2026-05-14 18:11:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
-function highlight() {
-    //Write your code here
+let arr=document.querySelectorAll("strong");
+        let link=document.querySelector("a");
+        link.addEventListener("mouseover",()=>{
+            highlight();
+        })
+        link.addEventListener("mouseout",()=>{
+            return_normal();
+        })
+        function highlight() {
+            arr.forEach(ele => {
+                ele.style.color="rgb(0, 128, 0)"
+            });
+        }
 
-
-}
-
-
-function return_normal() {
-    //Write your code here
-
-    
-}
+        function return_normal() {
+            arr.forEach(ele => {
+                ele.style.color="rgb(0, 0, 0)"
+            });
+        }
